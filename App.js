fe6285db895db32e3e79f398593c7b79ed154318
JongHeonLee <ismir1449@gmail.com>
2020-08-14 17:20:14 +0900

    success picker but it can't be changed

--- App.js
+++ App.js
@@ -7,31 +7,101 @@ import {
   View,
   ScrollView,
   StatusBar,
-  Picker,
+  SliderComponent,
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import {Picker} from '@react-native-community/picker';
 //스크린 임포트
 
 const axios = require('axios');
+
 class AuthBoardScreen extends Component {
   constructor(props) {
     super(props);
     console.log('Auth', this.props.data2);
   }
+  state = {
+    language: '001',
+    lang: 'java',
+    pick: [],
+    pickers: [],
+    data: [{id: 1, name: '시도'}],
+  };
 
+  // state = {picker: []};
   componentDidMount() {
+    console.log(this.state.language);
     axios.get('http://myks790.iptime.org:8082/board/major').then((response) => {
+      console.log(this.state.data[0]);
       this.setState({data: response.data});
-      // this.setState({data: '인증게시판'});
-      // console.log('auth', response.data);
+      const tmparr = [];
+      // 화살표함수를 써야 상위를 this로 부를 수 있음
+      console.log(this.state.data[0]);
+      this.state.data.map((item) => {
+        // return <Picker.Item label={item.name} value={item.id} />;
+        console.log(item.id, item.name);
+        tmparr.push(<Picker.Item label={item.id} value={item.name} />);
+      });
+      this.setState({pickers: tmparr});
     });
   }
+  putPickers() {
+    // if (this.state.pickers) {
+    //   console.log(this.state.pickers[0]);
+    //   return this.state.pickers[0];
+    // }
+    return (
+      <View>
+        <Picker
+          selectedValue={this.states.lang}
+          style={{height: 50, width: 100}}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({lang: itemValue})
+          }>
+          <Picker.Item label="Java" value="java" />
+          <Picker.Item label="JavaScript" value="js" />
+        </Picker>
+      </View>
+    );
+  }
+  addP = () => {
+    return (
+      <View>
+        {this.state.data.map((prop, key) => {
+          return <Picker.Item label={prop.id} key={key} value={prop.name} />;
+        })}
+      </View>
+    );
+  };
+  AddT = () => {
+    return (
+      <View>
+        <Text>{this.state.data[0].name}</Text>
+      </View>
+    );
+  };
+
   render() {
+    let pickerItems = this.state.data.map((item, i) => {
+      return <Picker.Item key={i} label={item.name} value={item.id.toString} />;
+    });
+
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>인증게시판입니다.</Text>
+
+        <this.AddT />
+        <Picker
+          selectedValue={this.state.language}
+          style={{height: 50, width: 100}}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({language: itemValue})
+          }>
+          {pickerItems}
+          {/* <addP /> */}
+        </Picker>
       </View>
     );
   }
